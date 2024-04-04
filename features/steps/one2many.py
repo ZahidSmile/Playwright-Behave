ac8490ba4a6a0f4c26c6694f6ff1bd5b5adb1a5b
ZahidSmile <zahid.ismail@savyour.com>
2024-04-04 16:23:04 +0500

    Update Hooks & Remove Unnecessary Code

--- features/steps/one2many.py
+++ features/steps/one2many.py
@@ -4,20 +4,17 @@ from selenium.common.exceptions import NoSuchElementException
 
 
 @given("the user is on the login page")
-# @async_run_until_complete
 async def step_impl(context):
     await context.page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
 
 
 @when('the user enters "{username}" and "{password}"')
-# @async_run_until_complete
 async def step_impl(context, username, password):
     await context.page.fill('input[name="username"]', username)
     await context.page.fill('input[name="password"]', password)
 
 
 @then('the user should be logged in successfully')
-# @async_run_until_complete
 async def step_impl(context):
     loginbtn = context.page.locator(".orangehrm-login-button")
     await loginbtn.click()
