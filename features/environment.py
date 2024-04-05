c3c1c05fda8ffe9a3c238662feedecc09ddc07cf
ZahidSmile <zahid.ismail@savyour.com>
2024-04-05 14:48:55 +0500

    Revert "Update the Behave Hook and remove unnecessary Code"
    This reverts commit 195750beba1f7096a8c4cad9d8ae449990a85257.

--- features/environment.py
+++ features/environment.py
@@ -1,11 +1,16 @@
-from playwright.sync_api import sync_playwright
+from behave import fixture, use_fixture
+from playwright.async_api import async_playwright
+from behave.api.async_step import async_run_until_complete
 
 
-def before_feature(context, feature):
-    context.browser = sync_playwright().start().chromium.launch(headless=False, slow_mo=500, channel="chrome")
-    context.page = context.browser.new_page()
+@fixture
+async def browser_chrome(context):
+    p = await async_playwright().start()
+    browser = await p.chromium.launch(headless=False, slow_mo=500, channel="chrome")  # Launching Chromium browser
+    context.page = await browser.new_page()
+    return context.page
 
 
-def after_feature(context, feature):
-    context.page.close()
-    context.browser.close()
+@async_run_until_complete
+async def before_scenario(context, scenario):
+    await use_fixture(browser_chrome, context)
--- features/steps/Login.py
+++ features/steps/Login.py
@@ -1,23 +1,28 @@
 from behave import given, when, then
+from behave.api.async_step import async_run_until_complete
 
 
 @given('I am on the login page')
-def step_impl(context):
-    context.page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+@async_run_until_complete
+async def step_impl(context):
+    await context.page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
 
 
 @when('I enter valid credentials')
-def step_impl(context):
-    context.page.fill('input[name="username"]', 'Admin')
-    context.page.fill('input[name="password"]', 'admin123')
+@async_run_until_complete
+async def step_impl(context):
+    await context.page.fill('input[name="username"]', 'Admin')
+    await context.page.fill('input[name="password"]', 'admin123')
 
 
 @when('I click the login button')
-def step_impl(context):
+@async_run_until_complete
+async def step_impl(context):
     loginbtn = context.page.locator('.orangehrm-login-button')
-    loginbtn.click()
+    await loginbtn.click()
 
 
 @then('I should be redirected to the dashboard')
-def step_impl(context):
+@async_run_until_complete
+async def step_impl(context):
     assert context.page.url == 'https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index'
